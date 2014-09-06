4d0b6f0dd777625b8d3ef19e10c8918e2039bc5d
Usman Majeed <majeedus@msu.edu>
2014-09-06 19:58:13 -0400

    Ready to push to Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
-Rails.application.routes.draw do
+Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +42,7 @@ Rails.application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-
+  
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
