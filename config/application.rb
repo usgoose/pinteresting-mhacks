715f8ef027e5c451f4f3bbb8ef0563b0a29914ee
Usman Majeed <majeedus@msu.edu>
2014-09-08 12:59:52 -0400

    Added user registration via Devise

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
+
 module Pinteresting
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -19,5 +21,6 @@ module Pinteresting
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+	config.assets.initialize_on_precompile = false
   end
 end
